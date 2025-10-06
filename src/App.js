780d7e30959e81539dc1c7fe0885d9646567c0b6
uhhlyy <martinallyson165@gmail>
2025-10-06 23:45:26 +0800

    login

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import './styles/navAndFooter.css';
 import './styles/jobs.css';
 import './styles/roleselection.css';
 import './styles/about.css';
+import './styles/login.css';
 
 import Index from './pages/index';
 import About from './pages/about';
 import JobsLanding from './pages/jobsLanding';
-import Roleselection from './pages/roleselection';  
+import Roleselection from './pages/roleselection'; 
+import Login from './pages/login'; 
 
 function App() {
   return (
@@ -23,6 +25,7 @@ function App() {
         <Route path="/about" element={<About />} /> 
         <Route path="/jobsLanding" element={<JobsLanding />} />
         <Route path="/roleselection" element={<Roleselection />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <FooterComponent />
     </Router>
